4aa0c651b8ed935db0136b5061f8563358ae155a
Guilherme Nathan <guilhermenathansp@gmail.com>
2018-01-10 22:05:11 -0200

    Desde o último commit fiz uma pancada de alterações (my bad really D:)

--- topics/script.js
+++ topics/script.js
@@ -1,5 +1,16 @@
+var cont = 0;
+
 function changePosition(){
 	
-	document.getElementByClassName("web-block").style.right = "100px";	
+	cont = cont + 1;
 
+	if(cont%2 != 0){
+		document.getElementById("wb").style.opacity = 1;
+		document.getElementById("wb").style.visibility = "visible";
+		document.getElementById("wb").style.transition = "visibility 0.3s, opacity 0.5s ease-out";
+			
+	}else{
+		document.getElementById("wb").style.visibility = "hidden";
+		document.getElementById("wb").style.opacity = 0;
+	}
 }
